2e124b3ec2bd34065ea311e2f63a5327ec322610
Diego Ordóñez <diego.ordonez@gmail.com>
2021-06-25 09:11:11 -0500

    Initial

---
+++ src/main/java/dordonez/servers/fotos_ws/FotosWsApplication.java
@@ -0,0 +1,13 @@
+package dordonez.servers.fotos_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotosWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotosWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dordonez/servers/fotos_ws/FotosWsApplicationTests.java
@@ -0,0 +1,13 @@
+package dordonez.servers.fotos_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotosWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
